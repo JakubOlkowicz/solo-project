aef845ca8ea22dd843a44c10d90e1edc966079ac
JakubOlkowicz <olkuba@poczta.fm>
2020-02-07 13:56:14 +0100

    finish page detail and add code validation js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 function burgerSidebar(){
   const btn = document.getElementById('menu-toggle');
-  // const lines = btn.querySelectorAll('.line');
   const cls = { open: 'open', close: 'close' };
   const sidebar = document.getElementById('side');
   const body = document.getElementById('body');
@@ -20,6 +19,59 @@ function burgerSidebar(){
     sidebar.classList.toggle('dis-block');
     body.classList.toggle('grid');
   });
-
 }
 burgerSidebar();
+
+function validation()                                    
+{ 
+  const surname = document.forms['RegForm']['Surname'];               
+  const name = document.forms['RegForm']['Name'];               
+  const email = document.forms['RegForm']['EMail'];    
+  const phone = document.forms['RegForm']['Telephone'];  
+  const password = document.forms['RegForm']['Password'];  
+  const repeat = document.forms['RegForm']['Repeat'];  
+
+  if (repeat.value == password.value)                                  
+  { 
+    window.alert('Please enter again password.'); 
+    repeat.focus(); 
+    return false; 
+  }
+
+  if (surname.value == '')                                  
+  { 
+    window.alert('Please enter your surname.'); 
+    surname.focus(); 
+    return false; 
+  }
+
+  if (name.value == '')                                  
+  { 
+    window.alert('Please enter your name.'); 
+    name.focus(); 
+    return false; 
+  }  
+       
+  if (email.value == '')                                   
+  { 
+    window.alert('Please enter a valid e-mail address.'); 
+    email.focus(); 
+    return false; 
+  } 
+   
+  if (phone.value == '')                           
+  { 
+    window.alert('Please enter your telephone number.'); 
+    phone.focus(); 
+    return false; 
+  } 
+   
+  if (password.value == '')                        
+  { 
+    window.alert('Please enter your password'); 
+    password.focus(); 
+    return false; 
+  } 
+  return true; 
+}
+validation();
