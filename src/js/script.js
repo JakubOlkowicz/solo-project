50e1808bca4d361d5f71c33d1ecb59e03ec4a62f
JakubOlkowicz <olkuba@poczta.fm>
2020-02-08 18:22:14 +0100

    add pop'up

--- src/js/script.js
+++ src/js/script.js
@@ -22,56 +22,111 @@ function burgerSidebar(){
 }
 burgerSidebar();
 
-function validation()                                    
-{ 
-  const surname = document.forms['RegForm']['Surname'];               
-  const name = document.forms['RegForm']['Name'];               
-  const email = document.forms['RegForm']['EMail'];    
-  const phone = document.forms['RegForm']['Telephone'];  
-  const password = document.forms['RegForm']['Password'];  
-  const repeat = document.forms['RegForm']['Repeat'];  
+// function validation()                                    
+// { 
+//   const surname = document.forms['RegForm']['Surname'];               
+//   const name = document.forms['RegForm']['Name'];               
+//   const email = document.forms['RegForm']['EMail'];    
+//   const phone = document.forms['RegForm']['Telephone'];  
+//   const password = document.forms['RegForm']['Password'];  
+//   const repeat = document.forms['RegForm']['Repeat'];  
 
-  if (repeat.value == password.value)                                  
-  { 
-    window.alert('Please enter again password.'); 
-    repeat.focus(); 
-    return false; 
-  }
+//   if (repeat.value == password.value)                                  
+//   { 
+//     window.alert('Please enter again password.'); 
+//     repeat.focus(); 
+//     return false; 
+//   }
 
-  if (surname.value == '')                                  
-  { 
-    window.alert('Please enter your surname.'); 
-    surname.focus(); 
-    return false; 
-  }
+//   if (surname.value == '')                                  
+//   { 
+//     window.alert('Please enter your surname.'); 
+//     surname.focus(); 
+//     return false; 
+//   }
 
-  if (name.value == '')                                  
-  { 
-    window.alert('Please enter your name.'); 
-    name.focus(); 
-    return false; 
-  }  
+//   if (name.value == '')                                  
+//   { 
+//     window.alert('Please enter your name.'); 
+//     name.focus(); 
+//     return false; 
+//   }  
        
-  if (email.value == '')                                   
-  { 
-    window.alert('Please enter a valid e-mail address.'); 
-    email.focus(); 
-    return false; 
-  } 
+//   if (email.value == '')                                   
+//   { 
+//     window.alert('Please enter a valid e-mail address.'); 
+//     email.focus(); 
+//     return false; 
+//   } 
    
-  if (phone.value == '')                           
-  { 
-    window.alert('Please enter your telephone number.'); 
-    phone.focus(); 
-    return false; 
-  } 
+//   if (phone.value == '')                           
+//   { 
+//     window.alert('Please enter your telephone number.'); 
+//     phone.focus(); 
+//     return false; 
+//   } 
    
-  if (password.value == '')                        
-  { 
-    window.alert('Please enter your password'); 
-    password.focus(); 
-    return false; 
-  } 
-  return true; 
+//   if (password.value == '')                        
+//   { 
+//     window.alert('Please enter your password'); 
+//     password.focus(); 
+//     return false; 
+//   } 
+//   return true; 
+// }
+// validation();
+
+function popUps(){
+  /* Icons */ 
+  const messagerIcon = document.getElementById('pop-messager');
+  const quitIcon = document.getElementById('pop-quit');
+  const profilIcon = document.getElementById('pop-profil');
+
+  /* Pops-up */
+  const  login = document.getElementById('login');
+  const quit = document.getElementById('quit');
+  /* Button */
+  const btnCancel = document.getElementById('btnCancel');
+  const btnQuit = document.getElementById('btnQuit');
+  
+  window.opener = self;
+  // Pop-Ups Login
+  profilIcon.addEventListener('click',function(){
+    login.style.display = 'flex';
+  });
+
+  // Pop-ups Quit
+  quitIcon.addEventListener('click', function(){
+    quit.style.display = 'flex';
+  });
+  btnCancel.addEventListener('click', function(){
+    quit.style.display = 'none';
+  });
+  btnQuit.addEventListener('click', function(){
+    window.close();
+    quit.style.display = 'none';
+  });
 }
-validation();
+popUps();
+
+function sidebarActive(){
+  const sidebars = document.querySelectorAll('.sidebar-menu');
+  for(let sidebar of sidebars){
+    sidebar.addEventListener('click', function(){
+      console.log('To ja', sidebar);
+      const idSidebar = sidebar.getAttribute('id');
+      const pages = document.querySelectorAll('container');
+      console.log('To ja', pages);
+      for(let page of pages){
+        const idPage = page.getAttribute('id');
+        console.log(idPage);
+        if(idSidebar === idPage){
+          console.log(page);
+          page.classList.toggle('.active');
+          sidebar.classList.toggle('.side-click');
+        }
+      }
+    });
+  }
+}
+sidebarActive();
